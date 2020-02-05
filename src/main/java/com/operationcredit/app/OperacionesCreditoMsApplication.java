c37bae49f9a1dbf983067cb265b0ee496df707bf
Jose Luis Eduardo Cayo Acuña <jcayoacu@everis.com>
2020-02-05 16:59:19 -0500

    Circuit braker añadido

--- src/main/java/com/operationcredit/app/OperacionesCreditoMsApplication.java
+++ src/main/java/com/operationcredit/app/OperacionesCreditoMsApplication.java
@@ -2,7 +2,10 @@ package com.operationcredit.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableCircuitBreaker
+@EnableEurekaClient
 @SpringBootApplication
 public class OperacionesCreditoMsApplication {
 
